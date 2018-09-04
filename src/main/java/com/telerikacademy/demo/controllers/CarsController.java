7b6f80450903ef85f49a5ef82d28e3150cdf4056
Slavi <slavi_14@abv.bg>
2018-09-04 18:05:48 +0300

    fifth commit

--- src/main/java/com/telerikacademy/demo/controllers/CarsController.java
+++ src/main/java/com/telerikacademy/demo/controllers/CarsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/api/cars")
 public class CarsController {
     private CarService service;
 
